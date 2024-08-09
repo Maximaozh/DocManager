7250d0203b8116d857de13da7a9192aa12869e60
Slastnikov Maksim <maximaozh@yandex.ru>
2024-08-09 15:06:05 +0700

    Добавьте файлы проекта.

---
+++ DocManager/DocManager/Program.cs
@@ -0,0 +1,33 @@
+using DocManager.Client.Pages;
+using DocManager.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(DocManager.Client._Imports).Assembly);
+
+app.Run();

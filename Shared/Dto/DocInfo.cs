79f1257b2b5cf410403a4d2bceb9e3a06c8a009e
Morozov Danil <danil_morozov2003@mail.ru>
2024-08-12 15:48:11 +0700

    Добавлена страница для работы с документом

---
+++ Shared/Dto/DocInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dto
+{
+    //Информация о документе
+    public class DocInfo
+    {
+        public required string Name { get; set; }
+        public required string Author { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
